d6c35c6ddc2c08b2914fee4457a60406ca58eee6
RiosGustavo <gustavosuperlango@gmail.com>
2023-08-12 15:41:59 -0500

    agragamos funcionalidad de eliminar producto

--- src/main/java/com/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -9,11 +9,13 @@ import com.ecommerce.model.Usuario;
 
 import com.ecommerce.service.ProductoService;
 import java.util.ArrayList;
+import java.util.Optional;
 import org.slf4j.*; //// con el * le decimos que importe todo lo que tiene este paquete 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -68,5 +70,34 @@ public class ProductoController {
         
     }
     
+    
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable String id, Model modelo){
+        
+        Producto producto = new Producto();
+        Optional<Producto> respuestaProducto = productoService.get(id);
+        producto = respuestaProducto.get();
+        
+        /// con lo sigueinte vemso en el output si nostrae el objeto buscado
+        LOGGER.info("Producto buscado: {}", producto);
+        modelo.addAttribute("producto", producto);
+        
+        
+        return "productos/edit";
+    }
+    
+    @PostMapping("/update")
+    public  String update(Producto producto){
+        
+        productoService.upDate(producto);
+        return "redirect:/productos";
+        
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable String id){
+        productoService.delete(id);
+        return "redirect:/productos";
+    }
     
 }
