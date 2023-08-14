5b2143218d63d0f36b245dfd4a821461d688ac94
RiosGustavo <gustavosuperlango@gmail.com>
2023-08-14 18:16:03 -0500

    producto vista  producto pagina usuario

---
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ecommerce.controller;
+
+
+import org.slf4j.Logger;
+import com.ecommerce.service.ProductoService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author User
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    
+    /// con esto nos impurime en al outload la trazabilidad poor si presenta algun error
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
+     
+    @Autowired
+    private ProductoService productoService;
+    
+    
+    
+    @GetMapping("")
+    public String home(Model modelo){
+        
+        modelo.addAttribute("productos", productoService.findAll());
+        
+        return "usuario/home";
+        
+    }
+    
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable String id){
+        log.info("id producto enviado como parametro {}", id);
+        
+        return "usuario/productohome";
+    }
+    
+    
+    
+}
