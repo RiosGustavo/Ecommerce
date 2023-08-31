0861139cb16daf9e835ca6068c61bc7a7d146377
RiosGustavo <gustavosuperlango@gmail.com>
2023-08-31 17:30:45 -0500

    ver detalles de las ordenes

--- src/main/java/com/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/ecommerce/controller/AdministradorController.java
@@ -4,7 +4,9 @@
  */
 package com.ecommerce.controller;
 
+import com.ecommerce.model.Orden;
 import com.ecommerce.model.Producto;
+import com.ecommerce.service.IOrdenService;
 import com.ecommerce.service.IUsuarioService;
 import com.ecommerce.service.ProductoService;
 import com.ecommerce.service.UsuarioServiceImpl;
@@ -13,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -22,29 +25,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
-    
+
     @Autowired
     private ProductoService productoService;
-    
+
     @Autowired
     private IUsuarioService usuarioService;
     
-    
+    @Autowired
+    private IOrdenService ordenService;
+
     @GetMapping("")
-    public String home(Model model){
-        
+    public String home(Model model) {
+
         List<Producto> productos = productoService.findAll();
-        
+
         model.addAttribute("productos", productos);
-        
+
         return "administrador/home.html";  /// hay que poner la ruta donde esta guardado el html en templates
     }
-    
+
     @GetMapping("/usuarios")
-    public String usuarios(Model modelo){
-        
-        modelo.addAttribute("usuarios", usuarioService.findAll() );
+    public String usuarios(Model modelo) {
+
+        modelo.addAttribute("usuarios", usuarioService.findAll());
         return "administrador/usuarios";
     }
+
+    @GetMapping("/ordenes")
+    public String ordenes(Model modelo) {
+        
+        modelo.addAttribute("ordenes", ordenService.findAll());
+        return "administrador/ordenes";
+    }
     
+     @GetMapping("/detalle/{id}")
+    public String detalle(Model modelo, @PathVariable String id){
+        
+        Orden orden = ordenService.findById(id).get();
+            
+        modelo.addAttribute("detalles", orden.getDetalle());
+        
+        return "administrador/detalleorden";
+        
+    }
+
 }
